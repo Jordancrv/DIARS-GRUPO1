5f01fd3a19a156b9712a1f279ef928e07371c368
Awzd23 <fruxjux@gmail.com>
2025-05-30 10:24:58 -0500

    Se agrego datProducto

---
+++ CapaDatos/datProducto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class datProducto
+    {
+        #region Singleton
+        private static readonly datProducto UnicaInstancia = new datProducto();
+        public static datProducto Instancia
+        {
+            get { return datProducto.UnicaInstancia; }
+        }
+        #endregion Singleton
+    }
+}
