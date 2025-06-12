d3c5d9255e37cd2beb5684c64b5acf9dccfe65c5
BenitesAnderson <ander_santi_24@hotmail.com>
2025-06-12 17:37:48 -0500

    arreglado

--- SantaEulalia/obj/Debug/net8.0/SantaEulalia.AssemblyInfo.cs
+++ SantaEulalia/obj/Debug/net8.0/SantaEulalia.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SantaEulalia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2897effa384f49cc8ce00aa9591dce8d764aded7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+215d47b80eaaf59d278f0b3ec3358aa68f82a011")]
 [assembly: System.Reflection.AssemblyProductAttribute("SantaEulalia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SantaEulalia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
