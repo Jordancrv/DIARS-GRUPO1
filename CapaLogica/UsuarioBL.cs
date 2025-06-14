242b2d60d2a4a83c4ec7a8dcf7f93755fee20aa5
Fabrii01 <fabrizioleturia5@gmail.com>
2025-06-14 18:32:11 -0500

    Incorporación del login al programa

---
+++ CapaLogica/UsuarioBL.cs
@@ -0,0 +1,31 @@
+﻿using CapaDatos;
+using CapaEntidad;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaLogica
+{
+    public class UsuarioBL
+    {
+        private static UsuarioBL instancia = null;
+        public static UsuarioBL Instancia
+        {
+            get
+            {
+                if (instancia == null)
+                    instancia = new UsuarioBL();
+                return instancia;
+            }
+        }
+
+        public entUsuario Login(string email, string password)
+        {
+            // Aquí llamas al DAO para validar
+            return datUsuario.Instancia.Login(email, password);
+        }
+    }
+
+}
