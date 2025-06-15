6b942fb3f79fcefa9a3a7c7a81469733ed43cbb9
BenitesAnderson <ander_santi_24@hotmail.com>
2025-06-15 01:26:22 -0500

    listo o no ?

--- SantaEulalia/obj/Debug/net8.0/SantaEulalia.AssemblyInfo.cs
+++ SantaEulalia/obj/Debug/net8.0/SantaEulalia.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SantaEulalia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0630c7e306e0c69dedc7cda6cb986326c94e43a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d318663ac94ec7c7dac75cdc03da1cb72f9c9f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("SantaEulalia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SantaEulalia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
