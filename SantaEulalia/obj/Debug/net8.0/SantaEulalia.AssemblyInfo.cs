b6cedf29e18880d952df648a20aff080e7326f2e
BenitesAnderson <ander_santi_24@hotmail.com>
2025-06-20 23:41:01 -0500

    cambios de fab bajados

--- SantaEulalia/obj/Debug/net8.0/SantaEulalia.AssemblyInfo.cs
+++ SantaEulalia/obj/Debug/net8.0/SantaEulalia.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SantaEulalia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b62cb52f37a8a08975144bf498e67fef90c6e83")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a13d1297068940b9e6187e2862c4a829ee79c417")]
 [assembly: System.Reflection.AssemblyProductAttribute("SantaEulalia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SantaEulalia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
